b2dc0d18f1f7be574a36ff1c537332ca985e0122
Alexander Surma <alexander.surma@gmail.com>
2012-02-19 01:15:58 +0100

    Git’d old code

---
+++ surmsig.c
@@ -0,0 +1,3 @@
+/* Alexander "crock" Surma */main(int n,char**a){char*t=*&n&1?"JPQR"/**/
+"N;==[TPQTNVLMNUORPQRORPTRVKSRSVL":1[a];int i,p=13;for(n=4,i=0;n+1;/***/
+printf("%c",((i[t]-59-i%5)&(1<<n))?35:32),!t[++i]?puts(&p),i=0,n--:0);};
